b7e3ad6bb5f9ee96333c36dc35328ccce349f04b
yuniors87 <yuniors87@gmail.com>
2018-10-16 23:27:54 -0400

    develop branch

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server running on http://localhost:${deets.port}`);
+  }
+);
